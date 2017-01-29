9efcf0466da28ec31a5f78a6a6f59bb6ad923232
KarapetianDV <karapetiandav@gmail.com>
2017-01-29 17:01:25 +0300

    Добавлен поиск по названиям стихов

--- app/src/main/java/com/example/davit/poems/PoemsAdapter.java
+++ app/src/main/java/com/example/davit/poems/PoemsAdapter.java
@@ -4,21 +4,29 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 
-public class PoemsAdapter extends RecyclerView.Adapter<PoemsAdapter.ViewHolder> {
+public class PoemsAdapter extends RecyclerView.Adapter<PoemsAdapter.ViewHolder> implements Filterable {
 
     private static final String TAG = PoemsAdapter.class.getSimpleName();
+
     private ArrayList<String> poemsNames;
     private String authorName;
+    private ArrayList<String> filteredList;
     private RecyclerItemClickListener listener;
+    private PoemsFilter poemsFilter;
 
     public PoemsAdapter(ArrayList<String> poemsNames, String authorName, RecyclerItemClickListener listener) {
         this.poemsNames = poemsNames;
+        this.filteredList = poemsNames;
         this.authorName = authorName;
         this.listener = listener;
+
+        getFilter();
     }
 
     @Override
@@ -31,13 +39,21 @@ public class PoemsAdapter extends RecyclerView.Adapter<PoemsAdapter.ViewHolder> 
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
-        holder.poemName.setText(poemsNames.get(position));
+        holder.poemName.setText(filteredList.get(position));
         holder.poemAuthor.setText(authorName);
     }
 
     @Override
     public int getItemCount() {
-        return poemsNames.size();
+        return filteredList.size();
+    }
+
+    @Override
+    public Filter getFilter() {
+        if (poemsFilter == null)
+            poemsFilter = new PoemsFilter();
+
+        return poemsFilter;
     }
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
@@ -49,6 +65,37 @@ public class PoemsAdapter extends RecyclerView.Adapter<PoemsAdapter.ViewHolder> 
             super(itemView);
             poemName = (TextView) itemView.findViewById(R.id.poemName);
             poemAuthor = (TextView) itemView.findViewById(R.id.poemAuthor);
+        }
+    }
+
+    private class PoemsFilter extends Filter {
+
+        @Override
+        protected FilterResults performFiltering(CharSequence constraint) {
+            FilterResults results = new FilterResults();
+
+            if (constraint != null && constraint.length() > 0) {
+                ArrayList<String> tempList = new ArrayList<>();
+
+                for (String poet : poemsNames) {
+                    if (poet.toLowerCase().contains(constraint.toString().toLowerCase()))
+                        tempList.add(poet);
+                }
+
+                results.values = tempList;
+                results.count = tempList.size();
+            } else {
+                results.values = poemsNames;
+                results.count = poemsNames.size();
+            }
+
+            return results;
+        }
+
+        @Override
+        protected void publishResults(CharSequence constraint, FilterResults results) {
+            filteredList = (ArrayList<String>) results.values;
+            notifyDataSetChanged();
         }
     }
 }
--- app/src/main/java/com/example/davit/poems/PoemsListActivity.java
+++ app/src/main/java/com/example/davit/poems/PoemsListActivity.java
@@ -3,10 +3,15 @@ package com.example.davit.poems;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 
 import org.jsoup.Jsoup;
@@ -22,12 +27,10 @@ import java.util.concurrent.ExecutionException;
 import static com.example.davit.poems.AuthorsActivity.AUTHOR_INTENT_NAME_TAG;
 import static com.example.davit.poems.AuthorsActivity.AUTHOR_INTENT_URL_TAG;
 
-// TODO: Добавить кнопку назад
-// TODO: Добавить название автора в ToolBar
-
-public class PoemsListActivity extends AppCompatActivity {
+public class PoemsListActivity extends AppCompatActivity implements SearchView.OnQueryTextListener {
 
     private static final String TAG = PoemsListActivity.class.getSimpleName();
+    PoemsAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -65,13 +68,39 @@ public class PoemsListActivity extends AppCompatActivity {
 
         ArrayList<String> poemsList = new ArrayList<>(poemsMap.keySet());
 
-        PoemsAdapter poemsAdapter = new PoemsAdapter(poemsList, authorName, new RecyclerItemClickListener() {
+        mAdapter = new PoemsAdapter(poemsList, authorName, new RecyclerItemClickListener() {
             @Override
             public void onItemClick(View v, int position) {
 
             }
         });
-        poemsRecyclerView.setAdapter(poemsAdapter);
+        poemsRecyclerView.setAdapter(mAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.poems_activity_menu, menu);
+
+        MenuItem searchMenuItem = menu.findItem(R.id.poemsSearch);
+        SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchMenuItem);
+        searchView.setOnQueryTextListener(this);
+
+        return true;
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        mAdapter.getFilter().filter(query);
+
+        return true;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String newText) {
+        mAdapter.getFilter().filter(newText);
+
+        return true;
     }
 
     class PoemsListParserTask extends AsyncTask<Void, Void, HashMap<String, String>> {
