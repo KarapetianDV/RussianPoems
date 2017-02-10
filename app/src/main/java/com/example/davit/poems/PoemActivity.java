09550cab45ebd92c066bd6061d8e180e8017013e
KarapetianDV <karapetiandav@gmail.com>
2017-02-10 15:37:39 +0300

    Увеличение шрифта стиха и сохранение в настройках

--- app/src/main/java/com/example/davit/poems/PoemActivity.java
+++ app/src/main/java/com/example/davit/poems/PoemActivity.java
@@ -1,11 +1,17 @@
 package com.example.davit.poems;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.util.TypedValue;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 import com.example.davit.poems.data.PoemContract;
@@ -22,6 +28,14 @@ public class PoemActivity extends AppCompatActivity {
 
     private String name;
     private String authorName;
+    private String TEXT_SIZE_CONST = "TEXT_SIZE_CONST";
+
+    private TextView poemText;
+    private TextView authorText;
+
+    private float poemTextSize = 14;
+
+    private SharedPreferences preferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,11 +65,61 @@ public class PoemActivity extends AppCompatActivity {
             e.printStackTrace();
         }
 
-        TextView poemText = (TextView) findViewById(R.id.poemText);
-        TextView authorText = (TextView) findViewById(R.id.authorText);
+        poemText = (TextView) findViewById(R.id.poemText);
+        authorText = (TextView) findViewById(R.id.authorText);
+
+        preferences = getPreferences(MODE_PRIVATE);
+        initTextSize();
 
         poemText.setText(text);
         authorText.setText(authorName);
+    }
+
+    private void initTextSize() {
+        if (preferences.contains(TEXT_SIZE_CONST)) {
+            poemTextSize = preferences.getFloat(TEXT_SIZE_CONST, 14);
+            poemText.setTextSize(TypedValue.COMPLEX_UNIT_SP, poemTextSize);
+            Log.d(TAG, "initTextSize: poemTextSize есть в preferences и значение: " + poemTextSize);
+        } else {
+            SharedPreferences.Editor editor = getPreferences(MODE_PRIVATE).edit();
+            editor.putFloat(TEXT_SIZE_CONST, 14);
+            editor.apply();
+            Log.d(TAG, "initTextSize: poemTextSize нет в preferences и мы пишем: " + poemTextSize);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.poem_activity_menu, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        SharedPreferences.Editor editor = getPreferences(MODE_PRIVATE).edit();
+
+        switch (item.getItemId()) {
+            case R.id.incText:
+                poemText.setTextSize(TypedValue.COMPLEX_UNIT_SP, poemTextSize += 2);
+                editor.putFloat(TEXT_SIZE_CONST, poemTextSize);
+                editor.apply();
+
+                Log.d(TAG, "onOptionsItemSelected: poemTextSize увеличили и записали - " + poemTextSize);
+
+                return true;
+            case R.id.decText:
+                poemText.setTextSize(TypedValue.COMPLEX_UNIT_SP, poemTextSize -= 2);
+                editor.putFloat(TEXT_SIZE_CONST, poemTextSize);
+                editor.apply();
+
+                Log.d(TAG, "onOptionsItemSelected: poemTextSize уменьшили и записали - " + poemTextSize);
+
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     private class QueryForText extends AsyncTask<Void, Void, Cursor> {
